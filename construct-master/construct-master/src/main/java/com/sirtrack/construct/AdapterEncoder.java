c1263a05b7e26ca08d7cea49e38d8c3bf954a1b9
nasir6 <16100247@lums.edu.pk>
2015-10-13 17:31:03 +0500

    sps and pps syntax implemented

--- construct-master/construct-master/src/main/java/com/sirtrack/construct/AdapterEncoder.java
+++
@@ -1,7 +0,0 @@
-package com.sirtrack.construct;
-
-import com.sirtrack.construct.lib.Containers.Container;
-
-public interface AdapterEncoder<T>{
-  public Object encode(T obj, Container context);
-}
